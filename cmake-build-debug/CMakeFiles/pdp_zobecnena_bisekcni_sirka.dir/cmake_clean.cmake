3ef957a08aff25ad81075a991580c548efeb710f
Jaroslav Hrách <me@jaroslavhrach.cz>
2017-03-11 00:36:15 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/pdp_zobecnena_bisekcni_sirka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdp_zobecnena_bisekcni_sirka.dir/main.cpp.o"
+  "pdp_zobecnena_bisekcni_sirka.pdb"
+  "pdp_zobecnena_bisekcni_sirka"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pdp_zobecnena_bisekcni_sirka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
