4cf61828641e8c78c08fc99e8ef22f9f6df4ec05
Jaroslav Hrách <me@jaroslavhrach.cz>
2017-03-13 13:31:08 +0100

    konecne to pocita

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <set>
 #include <fstream>
 
 using namespace std;
@@ -14,16 +15,80 @@ void help() {
     cout << "a = přirozené číslo, 5 =< a =< n/2" << endl;
 }
 
+ostream& operator<<(ostream& os, vector<int>& graph)
+{
+    os << '{';
+    for (int j = 0; j < graph.size(); ++j) {
+        if (j != graph.size() - 1) {
+            os << graph[j] << ", ";
+        } else {
+            os << graph[j];
+        }
+    }
+    os << "}" << endl;
+
+    return os;
+}
+
 void printGraph(vector<vector<bool>> graph, int nodes) {
     for (int i = 0; i < nodes; ++i) {
         for (int j = 0; j < nodes; ++j) {
-            cout << graph[i][j] << ", ";
+            cout << graph[i][j] << " ";
         }
         cout << endl;
     }
 }
 
+int price(vector<vector<bool>>& graph, vector<int> state) {
+    int sameEdges = 0;
+    int edges = 0;
 
+    for (int i = 0; i < state.size(); ++i) {
+        for (int j = 0; j < graph.size(); ++j) {
+            if ( graph[state[i]][j] == 1 ) edges++;
+        }
+        for (int j = i + 1; j < state.size(); ++j) {
+            if ( graph[state[i]][state[j]] == 1 ) sameEdges++;
+        }
+    }
+
+    return edges - sameEdges * 2;
+}
+
+bool BBDFS(uint a, vector<vector<bool>> graph, vector<int> state, uint &minPrice, uint depth, vector<int> &result) {
+
+    int firstNode;
+    int tmpPrice;
+
+    if ( depth < a ) {
+        if ( depth == 0 ) {
+            firstNode = 0;
+        } else {
+            firstNode = state[state.size() - 1] + 1;
+        }
+        for (int i = firstNode; i < graph.size(); ++i) {
+            state.push_back(i);
+            tmpPrice = price(graph, state);
+            if ( state.size() == a ) {
+                tmpPrice = price(graph, state);
+                if (tmpPrice < minPrice) {
+                    minPrice = tmpPrice;
+                    result = state;
+                }
+                //cout << tmpPrice << ": " << state;
+            }
+            if ( tmpPrice <= minPrice ) {
+                BBDFS(a, graph, state, minPrice, depth + 1, result);
+            }
+            state.pop_back();
+        }
+    } else {
+        return false;
+    }
+
+
+    return true;
+}
 
 int main(int argc, char const* argv[]) {
 
@@ -35,7 +100,7 @@ int main(int argc, char const* argv[]) {
     uint n = strtoul (argv[1], NULL, 0);
     uint k = strtoul (argv[2], NULL, 0);
     uint a = strtoul (argv[4], NULL, 0);
-    vector<vector<bool> > graph;
+    vector<vector<bool>> graph;
     vector<bool> graphLine;
     int nodes;
     string node;
@@ -58,10 +123,26 @@ int main(int argc, char const* argv[]) {
         }
     } else {
         cerr << "chyba při otevírání souboru" << endl;
+        return 0;
     }
 
     cout << "n = " << n << ", k = " << k << ", a = " << a << endl;
-    printGraph(graph, nodes);
+    //printGraph(graph, nodes);
+
+    uint edges = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if ( graph[i][j] == 1 ) edges++;
+        }
+    }
+    vector<int> state, result;
+    clock_t timeStart = clock();
+    BBDFS(a, graph, state, edges, 0, result);
+    double duration = ( clock() - timeStart ) / (double) CLOCKS_PER_SEC;
+
+    cout << "n = " << result;
+    cout << "edges = " << edges << endl;
+    cout << "time = " << duration << endl;
 
     inputFile.close();
 
